ce4180c1b935a272fa981e49bd7e96141b4b5b96
Sadjustanin <geigei16@mail.ru>
2023-09-16 18:56:35 +0500

    true

--- array1.php
+++ array1.php
@@ -6,6 +6,8 @@ for($i = 0; $i < rand(1,1000); $i++){
     $array1[$i] = mt_rand(1,1000);
 }
 
+echo "Я Заика Александр являюсь геем" ;
+
 // for($i = 0; $i < count($array1); $i++){
 //     echo $array1[$i] . "<br>";
 // }
