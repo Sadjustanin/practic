3970c50a2965f5e8425b0d7f35611613d8601fe7
CANE4KA <pony.123987@yandex.ru>
2023-09-16 18:24:30 +0500

    test

---
+++ array1.php
@@ -0,0 +1,13 @@
+<?php 
+
+$array1 = [];
+
+for($i = 0; $i < rand(1,1000); $i++){
+    $array1[$i] = mt_rand(1,1000);
+}
+
+// for($i = 0; $i < count($array1); $i++){
+//     echo $array1[$i] . "<br>";
+// }
+
+?>
---
+++ start.php
@@ -0,0 +1,9 @@
+<?php
+
+$array = array(
+    $array1 => array("red" => "красный", "blue"),
+    "array2"=> array("red", "blue"),
+    "array3"=> array("red", "blue")
+);
+
+?>
